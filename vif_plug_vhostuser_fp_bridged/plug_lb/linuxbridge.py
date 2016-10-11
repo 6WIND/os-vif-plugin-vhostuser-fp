440b771224b0d7a23a99ed86be5225fda4f54509
Francesco Santoro <francesco.santoro@6wind.com>
2016-10-11 11:29:20 +0200

    bridged_plugin: populate with lb fp plugin files
    This patch adds implementation for the fp plugin to plug/unplug
    fastpath vhostuser ports in linuxbridge.
    
    Signed-off-by: Francesco Santoro <francesco.santoro@6wind.com>

---
+++ vif_plug_vhostuser_fp_bridged/plug_lb/linuxbridge.py
@@ -0,0 +1,78 @@
+#    Copyright 2016 6WIND S.A.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_concurrency import processutils
+from oslo_config import cfg
+from oslo_log import log as logging
+
+from os_vif_plugin_vhostuser_fp import fp_plugin
+
+from vif_plug_vhostuser_fp_bridged import common
+from vif_plug_vhostuser_fp_bridged.plug_lb import linux_net
+
+
+LOG = logging.getLogger(__name__)
+
+
+class LinuxBridgeFpPlugin(fp_plugin.FpPluginBase):
+    """TBD
+
+    FP plugin to deal with fastpath vhostuser ports for linuxbridge
+    """
+
+    CONFIG_OPTS = (
+        cfg.IntOpt('network_device_mtu',
+                   default=1500,
+                   help='MTU setting for network interface.',
+                   deprecated_group="DEFAULT"),
+    )
+
+    def __init__(self, plugin_name):
+        super(LinuxBridgeFpPlugin, self).__init__(
+            plugin_name,
+            LinuxBridgeFpPlugin.CONFIG_OPTS)
+
+    def plug(self, vif, instance_info):
+        """TBD
+
+        Create and plug fastpath vhostuser port in bridge
+        """
+
+        mtu = self.config.network_device_mtu
+        try:
+            common.create_fp_dev(vif.vif_name, vif.path, vif.mode, mtu)
+        except Exception:
+            raise processutils.ProcessExecutionError()
+
+        try:
+            common.ensure_bridge(vif.bridge_name)
+            common.add_bridge_port(vif.bridge_name, vif.vif_name)
+        except Exception:
+            raise processutils.ProcessExecutionError()
+
+    def unplug(self, vif, instance_info):
+        """TBD
+
+        Unplug and delete fastpath vhostuser port in bridge
+        """
+
+        try:
+            linux_net.delete_bridge_port(vif.bridge_name, vif.vif_name)
+        except Exception:
+            raise processutils.ProcessExecutionError()
+
+        try:
+            common.delete_fp_dev(vif.vif_name)
+        except Exception:
+            raise processutils.ProcessExecutionError()
