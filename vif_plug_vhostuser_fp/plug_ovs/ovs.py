6WIND/os-vif-plugin-vhostuser-fp

commit cb8edc16a7da61e8c42e68a0ba49cc4b34da7a56

vif_plug_vhostuser_fp: use linux_net from nova os_vif
Let's use linux_net module from nova os_vif, like this we will have all
needed methods to manipulate with ovs bridges, VETH pairs, MTU and other
netdev's settings and we will also benefit from access to
os_vif.internal modules like ip, pyroute2, etc.

* vif_plug_vhostuser_fp/common.py: delete some methods, which have been
already implemented in vif_plug_ovs.linux_net;

* vif_plug_vhostuser_fp/plug_ovs/ovs.py: adapt _unplug_bridge method,
created VETH pair in this case will be properly deleted by
vif_plug_ovs.linux_net methods. vhostuser port will be deleted lately in
unplug method, which at first calls _unplug_bridge.

* vif_plug_vhostuser_fp/plug_ovs/ovs.py: add info message in unplug
method.

Signed-off-by: Valentina Krasnobaeva <valentina.krasnobaeva@6wind.com>
Acked-by: Maxime Leroy <maxime.leroy@6wind.com>

vif_plug_vhostuser_fp/plug_ovs/ovs.py
@@ -18,9 +18,9 @@ from oslo_log import log as logging
 from os_vif_plugin_vhostuser_fp import fp_plugin
 
 from vif_plug_ovs import constants
+from vif_plug_ovs import linux_net
 
 from vif_plug_vhostuser_fp import common
-from vif_plug_vhostuser_fp.plug_ovs import linux_net
 
 
 LOG = logging.getLogger(__name__)
@@ -78,14 +78,14 @@ class OvsFpPlugin(fp_plugin.FpPluginBase):
 
         v1_name, v2_name = self.get_veth_pair_names(vif)
 
-        common.ensure_bridge(vif.port_profile.bridge_name)
+        linux_net.ensure_bridge(vif.port_profile.bridge_name)
 
-        common.add_bridge_port(vif.port_profile.bridge_name, vif.vif_name)
+        linux_net.add_bridge_port(vif.port_profile.bridge_name, vif.vif_name)
 
         if not linux_net.device_exists(v2_name):
             linux_net.create_veth_pair(v1_name, v2_name,
                                        self.get_mtu(vif))
-            common.add_bridge_port(vif.port_profile.bridge_name, v1_name)
+            linux_net.add_bridge_port(vif.port_profile.bridge_name, v1_name)
             linux_net.ensure_ovs_bridge(vif.network.bridge,
                                         constants.OVS_DATAPATH_SYSTEM)
             self._create_vif_port(vif, v2_name, instance_info)
@@ -97,11 +97,9 @@ class OvsFpPlugin(fp_plugin.FpPluginBase):
         bridge, and delete both veth devices.
         """
         v1_name, v2_name = self.get_veth_pair_names(vif)
-        common.delete_bridge(vif.port_profile.bridge_name, v1_name)
+        linux_net.delete_bridge(vif.port_profile.bridge_name, v1_name)
         linux_net.delete_ovs_vif_port(vif.network.bridge, v2_name,
                                       timeout=self.config.ovs_vsctl_timeout)
-        # delete VETH pair
-        linux_net.delete_net_dev(v2_name)
 
     def plug(self, vif, instance_info):
         """TBD
@@ -132,6 +130,7 @@ class OvsFpPlugin(fp_plugin.FpPluginBase):
         """
 
         try:
+            LOG.info("Removing fastpath vhostuser port from ovs")
             if vif.port_profile.hybrid_plug:
                 self._unplug_bridge(vif, instance_info)
             else:

vif_plug_vhostuser_fp/common.py
@@ -15,55 +15,11 @@ from oslo_concurrency import processutils
 
 from fp_vdev_remote.vdev_utils import get_vdev_cmd
 
+from vif_plug_ovs import linux_net
 from vif_plug_vhostuser_fp import privsep
 
-import os
 
 FP_VDEV_CMD = None
-
-
-def set_device_mtu(dev, mtu):
-    """Set the device MTU."""
-    processutils.execute('ip', 'link', 'set', dev, 'mtu', mtu,
-                         check_exit_code=[0, 2, 254])
-
-
-def device_exists(device):
-    """Check if ethernet device exists."""
-    return os.path.exists('/sys/class/net/%s' % device)
-
-
-@privsep.vif_plug.entrypoint
-def ensure_bridge(bridge):
-    if not device_exists(bridge):
-        processutils.execute('brctl', 'addbr', bridge)
-        processutils.execute('brctl', 'setfd', bridge, 0)
-        processutils.execute('brctl', 'stp', bridge, 'off')
-        syspath = '/sys/class/net/%s/bridge/multicast_snooping'
-        syspath = syspath % bridge
-        processutils.execute('tee', syspath, process_input='0',
-                             check_exit_code=[0, 1])
-        disv6 = ('/proc/sys/net/ipv6/conf/%s/disable_ipv6' %
-                 bridge)
-        if os.path.exists(disv6):
-            processutils.execute('tee',
-                                 disv6,
-                                 process_input='1',
-                                 check_exit_code=[0, 1])
-
-
-@privsep.vif_plug.entrypoint
-def delete_bridge(bridge, dev):
-    if device_exists(bridge):
-        processutils.execute('brctl', 'delif', bridge, dev)
-        processutils.execute('ip', 'link', 'set', bridge, 'down')
-        processutils.execute('brctl', 'delbr', bridge)
-
-
-@privsep.vif_plug.entrypoint
-def add_bridge_port(bridge, dev):
-    processutils.execute('ip', 'link', 'set', bridge, 'up')
-    processutils.execute('brctl', 'addif', bridge, dev)
 
 
 @privsep.vif_plug.entrypoint
@@ -71,11 +27,11 @@ def create_fp_dev(dev, sockpath, sockmode_qemu, mtu):
     global FP_VDEV_CMD
     if FP_VDEV_CMD is None:
         FP_VDEV_CMD = get_vdev_cmd()
-    if not device_exists(dev):
+    if not linux_net.device_exists(dev):
         sockmode = 'client' if sockmode_qemu == 'server' else 'server'
         processutils.execute(FP_VDEV_CMD, 'add', dev, '--sockpath', sockpath,
                              '--sockmode', sockmode, run_as_root=True)
-        set_device_mtu(dev, mtu)
+        linux_net._set_device_mtu(dev, mtu)
         processutils.execute('ip', 'link', 'set', dev, 'up',
                              check_exit_code=[0, 2, 254])
 
@@ -83,7 +39,7 @@ def create_fp_dev(dev, sockpath, sockmode_qemu, mtu):
 @privsep.vif_plug.entrypoint
 def delete_fp_dev(dev):
     global FP_VDEV_CMD
-    if device_exists(dev):
+    if linux_net.device_exists(dev):
         if FP_VDEV_CMD is None:
             FP_VDEV_CMD = get_vdev_cmd()
         processutils.execute(FP_VDEV_CMD, 'del', dev)

vif_plug_vhostuser_fp/plug_lb/linuxbridge.py
@@ -18,8 +18,8 @@ from oslo_log import log as logging
 
 from os_vif_plugin_vhostuser_fp import fp_plugin
 
+from vif_plug_linux_bridge import linux_net
 from vif_plug_vhostuser_fp import common
-from vif_plug_vhostuser_fp.plug_lb import linux_net
 
 
 LOG = logging.getLogger(__name__)
@@ -57,8 +57,9 @@ class LinuxBridgeFpPlugin(fp_plugin.FpPluginBase):
 
         try:
             LOG.info("Plugging fastpath vhostuser port in bridge")
-            common.ensure_bridge(vif.port_profile.bridge_name)
-            common.add_bridge_port(vif.port_profile.bridge_name, vif.vif_name)
+            linux_net.ensure_bridge(vif.port_profile.bridge_name)
+            linux_net.add_bridge_port(vif.port_profile.bridge_name,
+                                      vif.vif_name)
         except Exception:
             raise processutils.ProcessExecutionError()
 

commit d23b391094123fe0db50e9e75c31912e376f1ae7

vif_plug_vhostuser_fp/ovs: fix ovs.unplug
This fixes a bug introduced in the commit cb8edc16a7da
("vif_plug_vhostuser_fp: use linux_net from nova os_vif")

When vif.port_profile.hybrid_plug is False, we should pass
delete_netdev=False to os_vif.linux_net.delete_ovs_vif_port() call,
because by default it sets this last parameter as True and deletes a
created vhostuser port with 'ip link del'. Earlier, we didn't have any
problems with 'ip link del vhostuser', because of DPVI kmod support.
Since VA 2.0 we no longer install any kernel modules, so dpdk vhostuser
port should be always deleted by 'fp-cli dpdk-port-virtual-del' command,
that allows to synchronize kernel netdev list with fp-rte shared memory.

Fixes cb8edc16a7da ("vif_plug_vhostuser_fp: use linux_net from nova
os_vif")
Signed-off-by: Valentina Krasnobaeva <valentina.krasnobaeva@6wind.com>
Acked-by: Maxime Leroy <maxime.leroy@6wind.com>

vif_plug_vhostuser_fp/plug_ovs/ovs.py
@@ -98,6 +98,9 @@ class OvsFpPlugin(fp_plugin.FpPluginBase):
         """
         v1_name, v2_name = self.get_veth_pair_names(vif)
         linux_net.delete_bridge(vif.port_profile.bridge_name, v1_name)
+        # v2_name will be deleted in linux_net.delete_ovs_vif_port,
+        # because by default the last parameter 'delete_netdev=True'
+        # v1_name is its VETH pair, will be auto removed by kernel
         linux_net.delete_ovs_vif_port(vif.network.bridge, v2_name,
                                       timeout=self.config.ovs_vsctl_timeout)
 
@@ -134,9 +137,12 @@ class OvsFpPlugin(fp_plugin.FpPluginBase):
             if vif.port_profile.hybrid_plug:
                 self._unplug_bridge(vif, instance_info)
             else:
+                # pass 'delete_netdev=False', because vhostuser port
+                # should be properly deleted later in common.delete_fp_dev
                 linux_net.delete_ovs_vif_port(
                     vif.network.bridge, vif.vif_name,
-                    timeout=self.config.ovs_vsctl_timeout)
+                    timeout=self.config.ovs_vsctl_timeout,
+                    delete_netdev=False)
         except Exception:
             raise processutils.ProcessExecutionError()
 